6ac570b7bfdedc8e48f06f28dd257791ac5e99c2
TakahiroHimi <takahiro.mth.1209@gmail.com>
2021-07-03 20:26:03 +0900

    storybook設定

---
+++ .storybook/main.js
@@ -0,0 +1,23 @@
+module.exports = {
+  stories: ["../**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  webpackFinal: async (baseConfig) => {
+    baseConfig.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        "style-loader",
+        {
+          loader: "css-loader",
+          options: {
+            importLoaders: 1,
+            modules: {
+              localIdentName: "[local]___[hash:base64:2]",
+            },
+          },
+        },
+        "sass-loader",
+      ],
+    });
+    return { ...baseConfig };
+  },
+};
---
+++ .storybook/preview.js
@@ -0,0 +1,12 @@
+import "../styles/global.scss";
+import "../styles/variables.scss";
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
