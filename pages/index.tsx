f11777d2c1cac9b771814a1b2d7c2b60a6a0ab16
TakahiroHimi <takahiro.mth.1209@gmail.com>
2021-07-03 20:03:07 +0900

    sass追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,9 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import styles from "./index.module.scss";
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+  <>
+    <button className={styles.button}>Click me</button>
+  </>
+);
 
-export default IndexPage
+export default IndexPage;
